774345ff6fae0d024f8e142583fb4b93f058319e
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-03-16 22:10:38 +0900

    see the heavy task blocks UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { heavyTask } from "./common";
+
+let status = "";
+const runOnUIThread = (e: any) => {
+  status = "running...";
+  heavyTask("UIThread");
+  status = "finished";
+};
 
 function App() {
+  const [count, setCount] = useState(1);
+  setTimeout(() => {
+    setCount(count + 1);
+  }, 100);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{count}</h1>
+        <button onClick={runOnUIThread}>on UI Thread</button>
+        <h1>{status}</h1>
       </header>
     </div>
   );
